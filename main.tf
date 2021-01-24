f51de5b541591eb8d0598995e26f1687678e7e33
blackcloudgeek <gumpfinest3341983@gmail.com>
2021-01-24 10:08:24 -0600

    change bucket policy

--- main.tf
+++ main.tf
@@ -24,22 +24,18 @@ resource "aws_s3_bucket" "b" {
   acl    = "public-read"
 
   policy = <<POLICY
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": [
-            "s3:GetObject",
-            ],
-          "Resource": [
-            "arn:aws:s3:::blackcloudgeeks-blog.com/*"
-            ]
-        }
-      ]
-    }
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::blackcloudgeeks-blog.com/*"   
+      }
+    ]
+  }
   POLICY
 
   website {
