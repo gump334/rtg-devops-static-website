c9ee4a6b6b5f1d1957cb10dd74a0d5c62786d0b6
blackcloudgeek <gumpfinest3341983@gmail.com>
2021-01-24 18:26:13 -0600

    added a policy file to replace raw json inline

--- main.tf
+++ main.tf
@@ -23,20 +23,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "blackcloudgeeks-blog.com"
   acl    = "public-read"
 
-  policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::blackcloudgeeks-blog.com/*"   
-      }
-    ]
-  }
-  POLICY
+  policy = file("policy.json")
 
   website {
     index_document = "index.html"
